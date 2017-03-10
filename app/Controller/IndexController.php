a8aa47ce349925df5a951c7c61f66ca8a7b03287
Tim Horie <tak@thorie.com>
2017-03-09 20:27:40 -0800

    Updated home page

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -6,15 +6,18 @@ use App\Controller\ViewController;
 use App\Util\Config;
 use App\Util\Session;
 use App\Model\Menu;
+use App\Model\Form\Login;
 
 class IndexController extends ViewController
 {
     private $session;
+    private $loginForm;
     
-    public function __construct(Config $config, View $view, Menu $menu, Session $session)
+    public function __construct(Config $config, View $view, Menu $menu, Session $session, Login $loginForm)
     {
         parent::__construct($config, $view, $menu);
         $this->session = $session;
+        $this->loginForm = $loginForm;
     }
     
     public function indexAction(array $unfilteredRequestParams)
@@ -22,10 +25,12 @@ class IndexController extends ViewController
         $this->view->addVars($unfilteredRequestParams);
     
         $vars = [
-        'message' => $this->session->getOnce('message'),
-        'mailMessage' => $this->session->getOnce('mailMessage'),
-        'loggedIn' => $this->session->get('loggedIn'),
-    ];
+            'message' => $this->session->getOnce('message'),
+            'mailMessage' => $this->session->getOnce('mailMessage'),
+            'loggedIn' => $this->session->get('loggedIn'),
+        ];
+        
+        $vars += $this->loginForm->getState();
     
         $this->view->render('index', $vars);
     }
--- tests/app/Controller/IndexControllerTest.php
+++ tests/app/Controller/IndexControllerTest.php
@@ -5,6 +5,7 @@ use App\Util\Config;
 use App\Util\View;
 use App\Model\Menu;
 use App\Util\Session;
+use App\Model\Form\Login;
 
 class IndexControllerTest extends \PHPUnit_Framework_TestCase
 {
@@ -17,6 +18,7 @@ class IndexControllerTest extends \PHPUnit_Framework_TestCase
             View::class,
             Menu::class,
             Session::class,
+            Login::class,
         ]);
         
         $this->mocks[Config::class]->expects($this->any())
@@ -27,7 +29,8 @@ class IndexControllerTest extends \PHPUnit_Framework_TestCase
             $this->mocks[Config::class],
             $this->mocks[View::class],
             $this->mocks[Menu::class],
-            $this->mocks[Session::class]
+            $this->mocks[Session::class],
+            $this->mocks[Login::class]
         );
     }
     
@@ -41,6 +44,10 @@ class IndexControllerTest extends \PHPUnit_Framework_TestCase
         $this->mocks[View::class]->expects($this->once())
                 ->method('render')
                 ->with('index', ['message' => null, 'mailMessage' => null, 'loggedIn' => null]);
+        
+        $this->mocks[Login::class]->expects($this->once())
+                ->method('getState')
+                ->willReturn([]);
         
         $indexController->indexAction($params);
     }
