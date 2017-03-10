ca617ffa044fa3c4ca5066ec144fe345beddaccb
Tim Horie <tak@thorie.com>
2017-03-09 19:36:37 -0800

    Changed founder in businesses table to bigint(20)

--- sql/businesses.sql
+++ sql/businesses.sql
@@ -2,16 +2,14 @@ CREATE TABLE IF NOT EXISTS `businesses` (
 `id` bigint(20) NOT NULL,
   `businessname` varchar(250) NOT NULL,
   `shortname` varchar(30) NOT NULL,
-  `founder` varchar(255) NOT NULL,
+  `founder` bigint(20) NOT NULL,
   `created_ts` datetime NOT NULL,
   `updated_ts` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE `businesses`
- ADD PRIMARY KEY (`id`), 
-
+ ADD PRIMARY KEY (`id`),
  ADD UNIQUE KEY `businessname` (`businessname`),
-
  ADD UNIQUE KEY `shortname` (`shortname`);
 
 ALTER TABLE `businesses`
