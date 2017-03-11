9144c827a896790e8fb858ccc056ef2f17b23d6c
Tim Horie <tak@thorie.com>
2017-03-11 12:54:21 -0800

    Updated aliases

--- aliases.sh
+++ aliases.sh
@@ -2,3 +2,5 @@
 
 alias p='vendor/bin/phpunit --stop-on-failure'
 alias pc='p --exclude-group db --coverage-html coverage'
+alias push='git push origin HEAD'
+alias sync-master="git fetch upstream && git checkout master && git merge upstream/master && git push origin HEAD"
